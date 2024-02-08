8f3cc2ffafe11da970362bb70b44ee6f55b40cb8
Alec Musasa <alecmus@live.com>
2024-02-08 07:33:34 +0200

    Added Spring Initializr code.

---
+++ src/main/java/com/github/alecmus/visibility_example/VisibilityExampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.alecmus.visibility_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisibilityExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisibilityExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/alecmus/visibility_example/VisibilityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alecmus.visibility_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisibilityExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
