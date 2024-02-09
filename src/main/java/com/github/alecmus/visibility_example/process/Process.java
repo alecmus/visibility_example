5150841c4bf775493ce8249fccfd1f8bd2c378cd
Alec Musasa <alecmus@live.com>
2024-02-09 16:31:18 +0200

    Added Process interface and implementation bean.

---
+++ src/main/java/com/github/alecmus/visibility_example/process/Process.java
@@ -0,0 +1,31 @@
+package com.github.alecmus.visibility_example.process;
+
+import java.util.Map;
+
+public interface Process {
+
+    static class Properties {
+        private Long instanceKey;
+        private String correlationKey;
+
+        public String getCorrelationKey() {
+            return correlationKey;
+        }
+
+        public void setCorrelationKey(String correlationKey) {
+            this.correlationKey = correlationKey;
+        }
+
+        public Long getInstanceKey() {
+            return instanceKey;
+        }
+
+        public void setInstanceKey(Long instanceKey) {
+            this.instanceKey = instanceKey;
+        }
+    }
+
+    Properties startProcess(String processId);
+    void sendMessage(String messageName, String correlationKey);
+    void addVariables(Long instanceKey, Map<String, Object> variables);
+}
---
+++ src/main/java/com/github/alecmus/visibility_example/process/ProcessImpl.java
@@ -0,0 +1,82 @@
+package com.github.alecmus.visibility_example.process;
+
+import io.camunda.zeebe.client.ZeebeClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Component
+public class ProcessImpl implements Process {
+
+    private final Logger log = LoggerFactory.getLogger(ProcessImpl.class);
+    private final ZeebeClient zeebeClient;
+
+    @Autowired
+    public ProcessImpl(ZeebeClient zeebeClient) {
+        this.zeebeClient = zeebeClient;
+    }
+
+
+    @Override
+    public Properties startProcess(String processId) {
+        log.debug("Starting process: '" + processId + "' ...");
+        Properties properties = new Properties();
+
+        try {
+            // create Zeebe process instance and add instanceUUID as a process variable
+            // for use as a message correlation key
+            final String instanceUUID = UUID.randomUUID().toString();
+            Map<String, Object> process_variables = Map.of("instanceUUID", instanceUUID);
+
+            long instanceKey = zeebeClient.newCreateInstanceCommand()
+                    .bpmnProcessId(processId)
+                    .latestVersion()
+                    .variables(process_variables)
+                    .send().join()
+                    .getProcessInstanceKey();
+
+            properties.setInstanceKey(instanceKey);
+            properties.setCorrelationKey(instanceUUID);
+
+            log.debug("Process '" + processId + "' started");
+        } catch (Exception e) {
+            log.debug("Failed to start process '" + processId + "': " + e.getMessage());
+        }
+
+        return properties;
+    }
+
+    @Override
+    public void sendMessage(String messageName, String correlationKey) {
+        try {
+            log.debug("Sending message '" + messageName + "' - correlationKey = " + correlationKey);
+
+            // send message to process
+            zeebeClient.newPublishMessageCommand()
+                    .messageName(messageName)
+                    .correlationKey(correlationKey)
+                    .send().join();
+
+            log.debug("Message '" + messageName + "' sent");
+        } catch (Exception e) {
+            log.debug("Failed to send message '" + messageName + ": " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void addVariables(Long instanceKey, Map<String, Object> variables) {
+        try {
+            log.debug("Adding variables to process: " + variables.keySet());
+
+            zeebeClient.newSetVariablesCommand(instanceKey)
+                    .variables(variables)
+                    .send().join();
+        } catch (Exception e) {
+            log.debug("Failed to add variables to process instance '" + instanceKey + "': " + e.getMessage());
+        }
+    }
+}
